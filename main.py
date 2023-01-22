5b009521dd849ccf2e945baca1eea9c100d2076a
Aryan Sandilya <87183199+Aryansan1999@users.noreply.github.com>
2023-01-23 00:54:05 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import pyautogui
+# move to specific position
+
+while True:
+    pyautogui.moveTo(200, 200, duration=1)
+    pyautogui.moveTo(400, 200, duration=1)
+    pyautogui.moveTo(400, 400, duration=1)
+    pyautogui.moveTo(200, 400, duration=1)
